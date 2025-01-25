86567dec1cc7edb500d4de94fdb2bb3864c0ee02
shutu <3321652135@qq.com>
2025-01-25 12:26:47 +0800

    dp

---
+++ Ditui/src/java/P1004.java
@@ -0,0 +1,23 @@
+package Ditui.src.java;
+
+import java.util.Scanner;
+
+public class P1004 {
+    public static void main(String[] args) {
+            // 操作数序列 123
+            // 123 132 213 231 321
+        //
+        Scanner scanner=new Scanner(System.in);
+        int n = scanner.nextInt();
+        long[]array=new long[n+1];
+        array[0]=1;
+        array[1]=1;
+        for (int i = 2; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                // 1-n 每个节点都当一次跟节点
+                array[i]+=array[j-1]*array[i-j];
+            }
+        }
+        System.out.println(array[n]);
+    }
+}
