708d363094918e3dad0e41d28891e318a951aadf
shutu <3321652135@qq.com>
2025-04-08 11:39:47 +0800

    蓝桥杯复习

---
+++ LanQiaoBei/P9/one.java
@@ -0,0 +1,9 @@
+package LanQiaoBei.P9;
+
+public class one {
+    public static void main(String[] args) {
+        // 1月1日 第一天
+        // 5月4日 第几天
+        System.out.println(31+28+31+30+4);
+    }
+}
---
+++ review/P15/one.java
@@ -0,0 +1,8 @@
+package review.P15;
+
+public class one {
+	public static void main(String[] args) {
+		long num=202420242024L/2*24;
+		System.out.println(num);
+	}
+}
