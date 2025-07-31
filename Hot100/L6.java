cde40c27a4a5c66aa9a54f974fe54e435ee326f6
shutu <3321652135@qq.com>
2025-07-31 10:17:50 +0800

    Hot100第6题复习

--- Hot100/L6.java
+++ Hot100/L6.java
@@ -9,6 +9,7 @@ import java.util.*;
  */
 public class L6 {
     public static void main(String[] args) {
+        threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
