b6f7cf3c0e4bbff4d99741d4180385f871402ac7
shutu <3321652135@qq.com>
2025-08-04 21:00:19 +0800

    feat：Hot100第8题

---
+++ Hot100/L8.java
@@ -0,0 +1,25 @@
+package Hot100;
+
+public class L8 {
+
+    public static void main(String[] args) {
+        lengthOfLongestSubstring("tmmzuxt");
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int[] count = new int[128];
+        int left=0;
+        int maxLength=0;
+        for (int right = 0; right < s.length(); right++) {
+            char index=s.charAt(right);
+            count[index]++;
+
+            while (count[index]>1){
+                char charLeft = s.charAt(left);
+                count[charLeft]--;
+                left++;
+            }
+            maxLength=Math.max(maxLength,right-left+1);
+        }
+        return maxLength;
+    }
+}
