8ac3a73ffa8e40d2c611cfe686eccd866698496f
shutu <3321652135@qq.com>
2025-10-19 10:56:20 +0800

    feat:Hot100-61-分割回文串

---
+++ Hot100/L61.java
@@ -0,0 +1,46 @@
+package Hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L61 {
+
+    public static void main(String[] args) {
+        List<List<String>> res = partition("aab");
+        for (List<String> list : res) {
+            System.out.println(list);
+        }
+    }
+
+    public static List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void backtrack(String s, int start, List<String> path, List<List<String>> res) {
+       if (start>=s.length()){
+           // 开始位置已经超过字符串长度了，说明已经找到一个结果了
+           res.add(new ArrayList<>(path));
+           return;
+       }
+        for (int i = start; i < s.length(); i++) {
+            String substring = s.substring(start, i + 1);
+            if (isPalindrome(substring)){
+                // 当前子串是回文字符串，可以在该位置剪切
+                path.add(substring);
+                backtrack(s,i+1,path,res);
+                // 不在当前位置剪切
+                path.remove(path.size()-1);
+            }
+        }
+    }
+
+    private static boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
