485f95c7674e827c6a77e9183bb10678dfd6fb21
shutu <3321652135@qq.com>
2025-12-20 10:07:04 +0800

    feat：L91

---
+++ Hot100/L91.java
@@ -0,0 +1,37 @@
+package Hot100;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class L91 {
+    public static void main(String[] args) {
+        System.out.println(new L91().uniquePathsByDP(2,3));
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][]dp = new int[m][n];
+        for (int i = 0; i <= n; i++) {
+            dp[0][i]=1;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[i][0]=1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n ; j++) {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    public int uniquePathsByDP(int m, int n) {
+        int[]dp = new int[n];
+        Arrays.fill(dp,1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j]=dp[j-1]+dp[j];
+            }
+        }
+        return dp[n-1];
+    }
+}
