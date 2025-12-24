c8652ccd75bb9e53cc3162c989603325cd14c677
shutu <3321652135@qq.com>
2025-12-24 17:29:08 +0800

    feat：复习L3

--- Hot100/L3.java
+++ Hot100/L3.java
@@ -4,25 +4,32 @@ import java.util.HashSet;
 
 public class L3 {
 
+    /**
+     * 解题思路：
+     * 判断一个元素的最长连续序列，就是从当前数字开始去找比它大的数字，每次差值为1，如果找到了就len++，每次记录当前的最大值
+     * 优化点：如果当前数存在比他小1的数，则跳过，因为比他小1的数和它是连续的，它计算出来的最大连续序列肯定比当前数大1
+     * @param nums
+     * @return
+     */
     public int longestConsecutive(int[] nums) {
-        // 使用HashSet存储所有数，用来判断该数是否存在
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-        int maxLen=0;
-        // 细节：遍历set中的元素会比遍历nums中的元素少
-        for (int current : set) {
-            if (set.contains(current - 1)) {
+        int maxLen = 0;
+        for (Integer current : set) {
+            if (set.contains(current-1)){
                 continue;
             }
-            int len = 1;
-            int search = current + 1;
-            while (set.contains(search)) {
+            // 从当前数开始向后找
+            int len=1;
+            int search=current+1;
+            maxLen=Math.max(maxLen,len);
+            while (set.contains(search)){
                 len++;
                 search++;
+                maxLen=Math.max(maxLen,len);
             }
-            maxLen = Math.max(len, maxLen);
         }
         return maxLen;
     }
