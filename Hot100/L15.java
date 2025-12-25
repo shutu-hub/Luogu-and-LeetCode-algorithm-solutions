968a3245d1b6f2d8df7b75165028c7a9779c2afa
shutu <3321652135@qq.com>
2025-12-25 19:52:44 +0800

    feat：复习L15

--- Hot100/L15.java
+++ Hot100/L15.java
@@ -1,6 +1,5 @@
 package Hot100;
 
-import java.util.Arrays;
 
 public class L15 {
 
@@ -10,16 +9,18 @@ public class L15 {
 
     /**
      * nums中元素向右移动k个位置
+     * 向右移动k个位置，在移动过程中可能超出数组长度，mod数组长度，就可以保证数组索引都在范围内
      * @param nums
      * @param k
      */
     public static void rotate(int[] nums, int k) {
-        int[]numsNew=new int[nums.length];
-        int n = nums.length;
-        // numsNew是nums向右移动k个位置的数组
+        int[]newArray=new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            numsNew[(i+k) % n]=nums[i];
+            int index =(i+k)%nums.length;
+            newArray[index]=nums[i];
         }
-        System.arraycopy(numsNew, 0, nums, 0, nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=newArray[i];
+        }
     }
 }
