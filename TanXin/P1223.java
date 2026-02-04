b8b8183189e07f613dff1e044da1f80d60bb2990
shutu <3321652135@qq.com>
2026-02-04 18:48:04 +0800

    feat：P1223

--- TanXin/P1223.java
+++ TanXin/P1223.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class P1223 {
     public static void main(String[] args) {
