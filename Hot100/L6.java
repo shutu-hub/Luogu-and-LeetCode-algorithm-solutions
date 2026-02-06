1939b679fe2b3a11682df60a7924a667c6411de9
shutu <3321652135@qq.com>
2026-02-06 19:24:03 +0800

    feat：L6

--- Hot100/L6.java
+++ Hot100/L6.java
@@ -3,17 +3,25 @@ package Hot100;
 import java.util.*;
 
 /**
+ * HOT100-6:三数之和
  * 本题暴力写法是三重循环找到符号条件的 i,j和k,然后再去重，时间复杂度为三次方
  * 对数组进行排序，采用双指针策略，将第二层循环和第三层循环处在一层，时间复杂度可降低为二次方
  * 在一层循环中通过 l和r变量寻找符合元素，通过判断相邻元素是否相等实现去重
  */
 public class L6 {
     public static void main(String[] args) {
-        threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        List<List<Integer>> lists = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        // -4 -1 -1 0 1 2
+        for (List<Integer> list : lists) {
+            for (Integer integer : list) {
+                System.out.print(integer+" ");
+            }
+            System.out.println();
+        }
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-       // i l r nums[l]+nums[r]=-nums[i]
+       // nums[l]+nums[r]=-nums[i]
         List<List<Integer>> lists = new ArrayList<>();
        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
