dc0fed591e676f3d00d131f76ec3a67cc8445f59
shutu <3321652135@qq.com>
2026-02-08 20:09:33 +0800

    feat：L8

--- Hot100/L8.java
+++ Hot100/L8.java
@@ -5,6 +5,7 @@ public class L8 {
     public static void main(String[] args) {
         lengthOfLongestSubstring("tmmzuxt");
     }
+
     public static int lengthOfLongestSubstring(String s) {
         int[] count = new int[128];
         int left=0;
@@ -12,7 +13,6 @@ public class L8 {
         for (int right = 0; right < s.length(); right++) {
             char index=s.charAt(right);
             count[index]++;
-
             while (count[index]>1){
                 char charLeft = s.charAt(left);
                 count[charLeft]--;
