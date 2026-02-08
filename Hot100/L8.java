c4920717ff6a13ad9b353844df17691d58c6e495
shutu <3321652135@qq.com>
2026-02-08 20:21:07 +0800

    feat：L8- 无重复字符的最长子串

--- Hot100/L8.java
+++ Hot100/L8.java
@@ -3,19 +3,26 @@ package Hot100;
 public class L8 {
 
     public static void main(String[] args) {
-        lengthOfLongestSubstring("tmmzuxt");
+        System.out.println(lengthOfLongestSubstring("tmmzuxt"));
     }
 
+    /**
+     * 无重复字符的最长子串
+     * @param s
+     * @return
+     */
     public static int lengthOfLongestSubstring(String s) {
-        int[] count = new int[128];
-        int left=0;
-        int maxLength=0;
+       int[] count = new int[128];
+       int left = 0;
+       int maxLength = 0;
         for (int right = 0; right < s.length(); right++) {
-            char index=s.charAt(right);
+            char index = s.charAt(right);
             count[index]++;
+            // 如果count[index]>1，则说明left-right之间，index字符存在重复
             while (count[index]>1){
-                char charLeft = s.charAt(left);
-                count[charLeft]--;
+                // 收缩左边界
+                char leftChar = s.charAt(left);
+                count[leftChar]--;
                 left++;
             }
             maxLength=Math.max(maxLength,right-left+1);
