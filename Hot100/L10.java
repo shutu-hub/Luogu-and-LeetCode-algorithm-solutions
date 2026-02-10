9dee6bbbe7e804899165c888ea56e88f3864f24d
shutu <3321652135@qq.com>
2026-02-10 21:04:20 +0800

    feat：L10

--- Hot100/L10.java
+++ Hot100/L10.java
@@ -7,11 +7,14 @@ public class L10 {
 
     public int subarraySum(int[] nums, int k) {
         int count=0;
+        // 前缀和数组
         int[]prefixSum=new int[nums.length];
+
         prefixSum[0]=nums[0];
         for (int i = 1; i < nums.length; i++) {
             prefixSum[i]=prefixSum[i-1]+nums[i];
         }
+
         for (int end = 0; end < nums.length; end++) {
             for (int start = 0; start <= end; start++) {
                 int sum=prefixSum[end]-prefixSum[start]+nums[start];
