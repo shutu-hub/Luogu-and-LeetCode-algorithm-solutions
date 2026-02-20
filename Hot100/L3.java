b54080c6bd4aad48f2e74616159a173358864db7
shutu <3321652135@qq.com>
2026-02-20 10:07:51 +0800

    feat：L3-最长连续序列

--- Hot100/L3.java
+++ Hot100/L3.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 public class L3 {
 
     /**
+     * HOT-3:最长连续序列
      * 解题思路：
      * 判断一个元素的最长连续序列，就是从当前数字开始去找比它大的数字，每次差值为1，如果找到了就len++，每次记录当前的最大值
      * 优化点：如果当前数存在比他小1的数，则跳过，因为比他小1的数和它是连续的，它计算出来的最大连续序列肯定比当前数大1
@@ -12,25 +13,24 @@ public class L3 {
      * @return
      */
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+       HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-        int maxLen = 0;
-        for (Integer current : set) {
-            if (set.contains(current-1)){
+        int res = 0;
+        for (int num : set) {
+            if (set.contains(num-1)){
                 continue;
             }
-            // 从当前数开始向后找
-            int len=1;
-            int search=current+1;
-            maxLen=Math.max(maxLen,len);
+            // 找比它大的连续数
+            int curLen=1;
+            int search=num+1;
             while (set.contains(search)){
-                len++;
+                curLen++;
                 search++;
-                maxLen=Math.max(maxLen,len);
             }
+            res=Math.max(res,curLen);
         }
-        return maxLen;
+        return res;
     }
 }
