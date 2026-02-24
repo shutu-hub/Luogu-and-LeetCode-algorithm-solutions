92c915b7f5e414b914c0b7b131f3fbcb104081b4
shutu <3321652135@qq.com>
2026-02-24 12:16:00 +0800

    feat：L7-接雨水

--- Hot100/L7.java
+++ Hot100/L7.java
@@ -1,5 +1,13 @@
 package Hot100;
 
+
+/**
+ * HOT100-7:接雨水
+ * 解题思路：
+ *  双指针策略，我们依次计算每个柱子的蓄水量，然后累加即可得出总蓄水量，每个柱子的蓄水量取决于两侧最大高度相对较低的那一个，
+ *  当左侧最大高度较低时，蓄水高度是由左侧柱子的高度决定，当前柱子和左侧最大高度的差值就是当前柱子的蓄水量
+ *  当右侧最大高度较低时，蓄水高度是由右侧柱子的高度决定，当前柱子和右侧最大高度的差值就是当前柱子的蓄水量
+ */
 public class L7 {
 
     public static void main(String[] args) {
@@ -9,18 +17,22 @@ public class L7 {
     public static int trap(int[] height) {
        int sum=0;
        int left=0,right=height.length-1;
-       int leftMax=0,rightMax=0;
-       while(left<right){
-           leftMax=Math.max(leftMax,height[left]);
-           rightMax=Math.max(rightMax,height[right]);
-           if (height[left]<=height[right]){
-               // 左边柱子高度小于右边柱子高度 这个柱子的蓄水量受左柱子影响 长为1 高度为左柱子 - 当前柱子、
-               // 每次计算柱子高度小的一侧 所以右边有比leftMax更高的柱子 所以leftMax-height[left]就是高
-               sum+=leftMax-height[left];
+       int leftMax=height[0],rightMax=height[right];
+       while (left<right){
+           if (leftMax<=rightMax){
                left++;
+               if (height[left]<leftMax){
+                   sum+=leftMax-height[left];
+               }else {
+                   leftMax=height[left];
+               }
            }else{
-               sum+=rightMax-height[right];
                right--;
+               if (height[right]<rightMax){
+                   sum+=rightMax-height[right];
+               }else{
+                   rightMax=height[right];
+               }
            }
        }
        return sum;
