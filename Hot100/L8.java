373a39bb24fd81722e405a1fab8df85b949e638b
shutu <3321652135@qq.com>
2026-02-25 21:25:23 +0800

    feat：L8-无重复字符的最长子串

--- Hot100/L8.java
+++ Hot100/L8.java
@@ -1,5 +1,13 @@
 package Hot100;
 
+
+/**
+ * HOT100-8:无重复字符的最长子串
+ * 解题思路：
+ * 滑动窗口思想，我们假设一个区间，包含左边界和右边界，每次右边界向右移动一个字符后，该字符可能和已有区间中的某个字符重复，
+ * 这时候需要移动左边界到该重复字符的右侧，这样就能排除掉之前的重复字符
+ * 每次扩展右边界和收缩左边界之后，区间为无重复字符的区间，更新最大大小。
+ */
 public class L8 {
 
     public static void main(String[] args) {
@@ -12,16 +20,16 @@ public class L8 {
      * @return
      */
     public static int lengthOfLongestSubstring(String s) {
-       int[] count = new int[128];
-       int left = 0;
-       int maxLength = 0;
-        for (int right = 0; right < s.length(); right++) {
-            char index = s.charAt(right);
-            count[index]++;
-            // 如果count[index]>1，则说明left-right之间，index字符存在重复
-            while (count[index]>1){
-                // 收缩左边界
-                char leftChar = s.charAt(left);
+        char[] chars = s.toCharArray();
+        int[] count = new int[128];
+        int left = 0;
+        int maxLength=0;
+        for (int right = 0; right < chars.length; right++) {
+            // 扩展右边界,收缩左边界
+            char rightChar = chars[right];
+            count[rightChar]++;
+            while (count[rightChar]>1){
+                char leftChar = chars[left];
                 count[leftChar]--;
                 left++;
             }
