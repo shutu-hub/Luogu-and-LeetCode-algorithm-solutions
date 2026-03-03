728249a9ce2a3345985fef9d7cd1867d66951aae
shutu <3321652135@qq.com>
2026-03-03 19:16:52 +0800

    feat：L10

--- Hot100/L10.java
+++ Hot100/L10.java
@@ -5,24 +5,24 @@ public class L10 {
 
     }
 
+    // -1 -1 1
+    // -1 -2 -1
     public int subarraySum(int[] nums, int k) {
-        int count=0;
-        // 前缀和数组
-        int[]prefixSum=new int[nums.length];
-
+        // 找到数组中和为 k 的子数组
+        // nums.length在1e4级别，时间复杂度最高为O(n2)
+        int[] prefixSum= new int[nums.length];
         prefixSum[0]=nums[0];
         for (int i = 1; i < nums.length; i++) {
             prefixSum[i]=prefixSum[i-1]+nums[i];
         }
-
-        for (int end = 0; end < nums.length; end++) {
-            for (int start = 0; start <= end; start++) {
-                int sum=prefixSum[end]-prefixSum[start]+nums[start];
-                if (sum==k){
-                    count++;
+        int sum=0;
+        for (int left = 0; left < nums.length; left++) {
+            for (int right = left; right < nums.length; right++) {
+                if (prefixSum[right]-prefixSum[left]+nums[left]==k){
+                    sum++;
                 }
             }
         }
-        return count;
+        return sum;
     }
 }
