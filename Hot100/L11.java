4f7a260bee43f6c25e6daed3491adb9672591643
shutu <3321652135@qq.com>
2026-03-04 20:37:49 +0800

    feat：L11

--- Hot100/L11.java
+++ Hot100/L11.java
@@ -3,34 +3,41 @@ package Hot100;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+/**
+ * HOT100-11:滑动窗口的最大值
+ */
 public class L11 {
 
     public static void main(String[] args) {
-        int[] nums = {1};
-        int[] ints = maxSlidingWindow(nums, 1);
+        int[] nums = {3,1,1,3};
+        int[] ints = maxSlidingWindow(nums, 3);
         System.out.println(Arrays.toString(ints));
     }
 
+    // 1 3 2 5 6
+    // 单调递减的队列，头部元素最大，尾部元素最小
+    // 插入元素时应保证单调性，插入元素要小于尾部元素
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        // 1 创建固定区间的窗口,存储元素下标
-        LinkedList<Integer> window = new LinkedList<>();
-        int[] result = new int[nums.length-k+1];
-        int index = 0;
-
+       LinkedList<Integer> queue = new LinkedList<>();
+       int[] res = new int[nums.length-k+1];
+       int left = 0;
         for (int right = 0; right < nums.length; right++) {
-            // 保证队列的有序性
-             while (window.peekLast()!=null && nums[right]>=nums[window.peekLast()]){
-                    window.pollLast();
-             }
-             window.offer(right);
-             // 排除已经过期的元素
-             while (window.peekFirst()!=null && right-window.peekFirst()>=k){
-                 window.pollFirst();
-             }
-             if (right>=k-1){
-                 result[index++]=nums[window.peekFirst()];
-             }
+            // 注意这里比较的是值
+            while (!queue.isEmpty() && nums[queue.peekLast()]<nums[right]){
+                queue.pollLast();
+            }
+            // 将新元素插入到队列
+            queue.offerLast(right);
+            // 判断头部有效性
+            while (queue.peekFirst()<=right-k){
+                queue.pollFirst();
+            }
+            // 记录窗口最大值
+            if (right>=k-1){
+                res[left]=nums[queue.peekFirst()];
+                left++;
+            }
         }
-        return result;
+        return res;
     }
 }
