0b3d62cca352d8cdf784cbe5879fb9147590a5fd
shutu <3321652135@qq.com>
2026-03-15 11:10:38 +0800

    feat：L21

--- Hot100/L21.java
+++ Hot100/L21.java
@@ -7,18 +7,15 @@ public class L21 {
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
-        // 从右上角开始搜索,标记当前位置为 x,y
-        // 如果当前元素大于搜索元素，则当前元素所在列元素全部大于搜索元素，列-1
-        // 如果当前元素小于搜索元素，则可以沿着这一列向下搜索，行+1
-        int x=0,y=n-1;
-        while (x<m && y>=0){
+        int x=0,y=matrix[0].length-1;
+        while (x<matrix.length && y>=0){
             if (matrix[x][y]==target){
                 return true;
-            }else if (matrix[x][y]>target){
-                y--;
-            }else{
+            }
+            if (matrix[x][y]<target){
                 x++;
+            }else{
+                y--;
             }
         }
         return false;
