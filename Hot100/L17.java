f067e44cea6752016cece00c343270b741ff4172
shutu <3321652135@qq.com>
2026-03-17 21:41:19 +0800

    feat：L17

--- Hot100/L17.java
+++ Hot100/L17.java
@@ -3,36 +3,32 @@ package Hot100;
 
 public class L17 {
     public static void main(String[] args) {
-        System.out.println(firstMissingPositive(new int[]{3,4,-1,1}));
+        System.out.println(firstMissingPositive(new int[]{1,1}));
     }
 
+    //  3 4  5  1
+    // -1 4 -3 -4
     public static int firstMissingPositive(int[] nums) {
+       // 长度为1-n的数组，其最小正整数必然在 1-n+1
+       // 数组长度为1-n，那就把这个数组当作为map，遍历数组如果这个数在1-n范围内，则将其改为-nums[i]
+       // 最后检查数组，如果nums[i]=负数，则这个正数存在，如果等于正数则这个数就是缺失的最小正数
         int n = nums.length;
-        // nums的长度 1-n,则缺失的第一个正数属于[1,n+1]
         for (int i = 0; i < nums.length; i++) {
             if (nums[i]<=0){
                 nums[i]=n+1;
             }
         }
-        // 3 4 5 1
-        // 3 4 -5 1
-        // 将数组变成哈希表
-        for (int i = 0; i < n; i++) {
-            int num = Math.abs(nums[i]);
-            // 如果 num 在 1-n这个范围，则打上标记
-            // 比如 num = 3,则对应数组中索引2的位置应该打上标记，表明2这个数存在，但是在打标记的时候可能会覆盖原有的数，所以在打标记的时候把数值取负数
-            // nums[num - 1]会出现已经打了标记，是负数的情况，如果直接取反，则会变成正数，所以要取绝对值
-            if (num<=n){
-                nums[num-1]=-Math.abs(nums[num - 1]);
+        for (int i = 0; i < nums.length; i++) {
+            int abs = Math.abs(nums[i]);
+            if (abs<=n){
+                nums[abs-1]= -Math.abs(nums[abs-1]);
             }
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i]>0){
-                // i+1就是缺失的正数
-                return i+1;
+                return i+1;// i从0开始，需要+1映射
             }
         }
-        // 遍历完数组，1-n都存在
         return n+1;
     }
 }
