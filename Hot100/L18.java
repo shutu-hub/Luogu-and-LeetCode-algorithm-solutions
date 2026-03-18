a7ae0055119001f2dfc567e59f8aeb31ee5c1aa9
shutu <3321652135@qq.com>
2026-03-18 20:22:45 +0800

    feat：L18

--- Hot100/L18.java
+++ Hot100/L18.java
@@ -7,23 +7,16 @@ public class L18 {
     }
 
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
+        // 直接暴力修改的话是3层循环，需要降低一层循环，使用标记的方法
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j]==0){
-                    // 所在行和列打上标记
-                    row[i]=true;
-                    col[j]=true;
-                }
-            }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (row[i]||col[j]){
-                    matrix[i][j]=0;
+                    for (int k = 0; k < matrix[i].length; k++) {
+                        matrix[i][k]=0;
+                    }
+                    for (int k = 0; k < matrix.length; k++) {
+                        matrix[k][j]=0;
+                    }
                 }
             }
         }
