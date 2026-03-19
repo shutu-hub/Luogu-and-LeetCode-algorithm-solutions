da787ff9ab075b5c995899b9d4ff857585242a57
shutu <3321652135@qq.com>
2026-03-19 19:24:13 +0800

    feat：L19

--- Hot100/L19.java
+++ Hot100/L19.java
@@ -7,55 +7,56 @@ public class L19 {
     public static void main(String[] args) {
         List<Integer> list = spiralOrder(new int[][]{{1, 2, 3,4}, {5, 6,7,8}, {9,10,11,12}});
         for (Integer ele : list) {
-            System.out.print(ele);
+            System.out.print(ele+" ");
         }
-
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        int top=0;
-        int bottom=matrix.length-1;
-        int left=0;
-        int right=matrix[0].length-1;
-        ArrayList<Integer> list =new ArrayList<>();
-        int maxCount=matrix.length*matrix[0].length;
-        int count=0;
-        while (count<maxCount){
-            // 从左边到右边
-            for (int i = left; i <= right ; i++) {
-                list.add(matrix[top][i]);
-                count++;
+        int top = 0;
+        int bottom = matrix.length-1;
+        int right = matrix[0].length-1;
+        int left = 0;
+        int cur = 0;
+        int maxCount = matrix.length * matrix[0].length;
+        List<Integer> res = new ArrayList<>();
+        while (cur<=maxCount){
+            // 从左向右走
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+                cur++;
             }
-            if (count==maxCount){
+            if (cur==maxCount){
                 break;
             }
             top++;
-            // 从上边到下边
+            // 从上向下走
             for (int i = top; i <= bottom ; i++) {
-                list.add(matrix[i][right]);
-                count++;
+                res.add(matrix[i][right]);
+                cur++;
             }
-            if (count==maxCount){
+            if (cur==maxCount){
                 break;
             }
             right--;
-            // 从右边到左边
-            if (right>left){
-                for (int i = right; i >=left ; i--) {
-                    list.add(matrix[bottom][i]);
-                    count++;
-                }
-                bottom--;
+            // 从右向左走
+            for (int i = right; i >=left ; i--) {
+                res.add(matrix[bottom][i]);
+                cur++;
             }
-          if (bottom>top){
-              // 从下边到上边
-              for (int i = bottom; i >=top ; i--) {
-                  list.add(matrix[i][left]);
-                  count++;
-              }
-              left++;
-          }
+            if (cur==maxCount){
+                break;
+            }
+            bottom--;
+            // 从下往上走
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+                cur++;
+            }
+            if (cur==maxCount){
+                break;
+            }
+            left++;
         }
-        return list;
+        return res;
     }
 }
