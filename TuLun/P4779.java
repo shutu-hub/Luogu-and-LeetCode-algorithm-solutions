dcfb5d22e5efbe25f3a9866ef2b044097edc80f2
shutu <3321652135@qq.com>
2026-04-05 18:47:34 +0800

    feat：L4779-迪杰斯特拉算法

--- TuLun/P4779.java
+++ TuLun/P4779.java
@@ -6,68 +6,86 @@ import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 import java.util.*;
 
+
 public class P4779 {
     public static void main(String[] args) throws IOException {
-        StreamTokenizer st=new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
         st.nextToken();
-        int n =(int)st.nval;
+        int n = (int) st.nval;
         st.nextToken();
-        int m = (int)st.nval;
+        int m = (int) st.nval;
         st.nextToken();
-        int s = (int)st.nval;
-        List<edge>[]graphs=new List[n+1];
-        for (int i = 0; i < graphs.length; i++) {
-            graphs[i]=new ArrayList<>();
+        int s = (int) st.nval;
+        List<Edge>[] graphs = new ArrayList[n+1];
+        for (int i = 0; i <= n; i++) {
+            graphs[i] = new ArrayList<>();
         }
-        for (int i = 1; i <= m; i++) {
+        for (int i = 0; i < m; i++) {
             st.nextToken();
-            int u = (int)st.nval;
+            int ui = (int) st.nval;
             st.nextToken();
-            int v = (int)st.nval;
+            int vi = (int) st.nval;
             st.nextToken();
-            long w = (int)st.nval;
-            graphs[u].add(new edge(v,w));
+            int wi = (int) st.nval;
+            graphs[ui].add(new Edge(vi,wi));
         }
-        long[]dis=new long[n+1];
-        for (int i = 1; i <= n; i++) {
+        long[]dis = new long[n+1];
+        for (int i = 0; i < dis.length; i++) {
             dis[i]=Integer.MAX_VALUE;
         }
         dis[s]=0;
-       PriorityQueue<long[]>queue=new PriorityQueue<>(Comparator.comparingLong(longs -> longs[1]));
-       // 到哪个节点，距离
-       queue.add(new long[]{s,0});
-       boolean[]visited=new boolean[n+1];
-       while (!queue.isEmpty()){
-           long[] arr = queue.poll();
-           int node = (int)arr[0];
-           long curDis = arr[1];
-
-           if (visited[node])continue;
-           visited[node]=true;
-
-           // 遍历该节点的邻接点 然后更新start到其邻接点的距离
-           for (edge edge : graphs[node]) {
-               int nextNode = edge.to;
-               long nextDis = edge.weight;
-               if (dis[nextNode]>curDis+nextDis){
-                   dis[nextNode]=curDis+nextDis;
-                   queue.offer(new long[]{nextNode,dis[nextNode]});
-               }
-           }
-       }
-        for (int i = 1; i <=n ; i++) {
+        // 默认小顶堆，反过来大顶堆
+        PriorityQueue<long[]> queue = new PriorityQueue<>((o1, o2) -> (int) (o1[1]-o2[1]));
+        queue.add(new long[]{s,0});
+        boolean[]flag = new boolean[n+1];
+        while (!queue.isEmpty()){
+            long[] poll = queue.poll();
+            int nextNode = (int) poll[0];
+            int nextDis = (int) poll[1];
+            if (flag[nextNode]){
+                continue;
+            }
+            flag[nextNode]=true;
+            for (int i = 0; i < graphs[nextNode].size(); i++) {
+                Edge edge = graphs[nextNode].get(i);
+                if (dis[edge.to]>nextDis+edge.weight){
+                    // 更新
+                    dis[edge.to]=nextDis+edge.weight;
+                    queue.offer(new long[]{edge.to,dis[edge.to]});
+                }
+            }
+        }
+        for (int i = 1; i <= n ; i++) {
             System.out.print(dis[i]+" ");
         }
     }
 
-    public static class edge{
+    static class Edge {
         int to;
         long weight;
 
-        public edge(int to, long weight) {
+        public Edge(int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
+
+        public int getTo() {
+            return to;
+        }
+
+        public void setTo(int to) {
+            this.to = to;
+        }
+
+        public long getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
     }
+
+
 
 }
